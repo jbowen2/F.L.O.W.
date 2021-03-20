e534d1e0a95eb462820460d84bc9993784d9ca36
John WIlliam Bowen <johnwb1776@gmail.com>
2021-03-20 00:24:42 +0000

    hold

--- bot/commands/prefix/vacation.js
+++ bot/commands/prefix/vacation.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
-const { Message } = require('discord.js');
+const { Message, MessageEmbed } = require('discord.js');
+const con = mysql.createConnection(process.env.MYSQLSERVER);
 module.exports = {
 	name: 'vacation',
 	description: 'For aproving and ending vacation',
@@ -10,30 +11,102 @@ module.exports = {
      * @param { string[] } args
      */
 	execute(message, args) {
-        message.delete();
-        const con = mysql.createConnection(process.env.MYSQLSERVER);
-        if(args[0].startsWith('<@')){
-            args[0] = args[0].slice(3,-1)
-            if (args[0].startsWith('!')) {
-                args[0] = args[0].slice(1);
-            }
-            const member = message.guild.members.resolve(args[0]);
-            con.query(`SELECT vacation_channel_id FROM guild WHERE guild_id = '${message.guild.id}'`,function(err, result){
-                if (err) throw err;
-                const channel = message.guild.channels.resolve(result[0].Vacation_channel_id);
-                con.query(`SELECT * FROM users WHERE discord_id = '${member.id}' AND guild_is = '${message.guild.id}'`, function (err, result) {
-                    if (err) throw err;
-                    if(result[0].Vacation){
-                        channel.send(`${member} is no longer on vacation`);
-                        console.log(`${member.displayName} is no longer on vacation`);
-                        con.query(`UPDATE users SET Vacation = false WHERE Discord_id= '${member.id}' AND guild_is = '${message.guild.id}'`,function (err) {if (err) throw err;});
-                    } else {
-                        channel.send(`${member} is on vacation`);
-                        console.log(`${member.displayName} is on vacation`);
-                        con.query(`UPDATE users SET Vacation = true WHERE Discord_id = '${member.id}' AND guild_is = '${message.guild.id}'`,function (err) {if (err) throw err;});
-                    }     
-                });
-            });       
+        message.reply('vaction System is WIP')
+        /**const opt = args[0];
+        const target = args[1];
+        const guild = message.guild;
+        if(!isEnabled(guild)){
+            const embed = new MessageEmbed()
+                .setAuthor('F.L.O.W.')
+                //.setColor() set later
+                .setTimestamp()
+                .setTitle('Vacation System')
+                .setDescription('This system provids a way of taking those how are out of the office.\n To enable the Vaction run `vacation enable`\n A channel and role will')
+                .addFields(
+                    {name: 'Status', value: 'Disabled'},
+                    {name: 'Not on Vaction', value: notVaction},
+                );
         }
+        con.query(`SELECT vaction_enable FROM guild WHERE guild_id =''${guild.id}`, (err,result) => {
+            if(err) throw err;
+            const vactionEnable = result[0].vaction_enable;
+            if(!vactionEnable){
+                message.reply('Vaction is not enabled.\n To enable run `');
+            }
+
+        });
+        if(!opt){
+            con.query(`SELECT discord_id, on_vacation FROM user WHERE guild_id='${guild.id}'`, (err, result) => {
+                if(err) throw err;
+                let vaction = '';
+                let notVaction = ';';
+                result.forEach(user => {
+                    if(user.on_action){
+                        vaction += `\n${guild.members.resolve(user.Discord_id).displayName}`;
+                    } else {
+                        notVaction +=`\n${guild.members.resolve(user.Discord_id).displayName}`;
+                    }
+                });
+                const embed = new MessageEmbed()
+                .setAuthor('F.L.O.W.')
+                //.setColor() set later
+                .setTimestamp()
+                .setTitle('Vaction Report')
+                .addFields(
+                    {name: 'On Vaction', value: vaction},
+                    {name: 'Not on Vaction', value: notVaction},
+                );
+                message.reply('',embed);
+            });
+        }else if(!target || !target.startsWith('<@')){
+            const add = '`vacation add @user`';
+            const remove = '`vacation remove @user`';
+            const embed = new MessageEmbed()
+                .setAuthor('F.L.O.W.')
+                //.setColor() set later
+                .setTimestamp()
+                .setTitle('Invalde Agrument or operator')
+                .setDescription('use one of the given operator')
+                .addFields(
+                    {name: 'add', value: vaction},
+                    {name: 'remove', value: notVaction},
+                );
+                message.reply('',embed);
+        } else {
+            const member = message.guild.members.resolve(target);
+            con.query(`
+                SELECT gc.vacation_channel_id, gr.vaction_channel_id, u.on_vaction 
+                FROM guild_channels AS gc, guild_role AS gr, user AS u
+                WHERE gc.guild_id = '${guild.id}
+                AND gr.guild_id = gc.guild_id 
+                AND u.discord_id='${member.id}'`, (err, result) =>{
+                if(err) throw err;
+                const vactionChannel = guild.channels.resolve(result[0].vacation_channel_id);
+                const vactionrole = guild.roles.resolve(result[0].vaction_role_id);
+                const onVaction = result.on_vaction;
+                if(onVaction){
+                    vactionChannel.send(`${member} is no longer on vacation`);
+                    member.roles.remove(vactionrole);
+                    console.log(`${member.displayName} is no longer on vacation`);
+                    con.query(`
+                        UPDATE users 
+                        SET Vacation = false 
+                        WHERE discord_id= '${member.id}' 
+                        AND guild_id = '${guild.id}'`,
+                        (err) => {if (err) throw err;});
+                } else {
+                    vactionChannel.send(`${member} is on vacation`);
+                    member.roles.add(vactionrole);
+                    console.log(`${member.displayName} is on vacation`);
+                    con.query(`
+                        UPDATE users 
+                        SET Vacation = true 
+                        WHERE discord_id = '${member.id}' 
+                        AND guild_id = '${guild.id}'`,
+                        (err) => {if (err) throw err;});
+                }
+                
+            });
+        }      */    
     },
 };
